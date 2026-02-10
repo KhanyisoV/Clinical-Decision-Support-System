d0582e28d78ecf59f4687f7ed17975c109e988f6
KhanyisoV <khanyisovabaza@gmail.com>
2026-02-10 21:07:18 +0200

    Fix API URL frontend

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 ﻿import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://corresponding-bibbye-khanyisov-2148b48e.koyeb.app/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://corresponding-bibbye-khanyisov-2148b48e.koyeb.app',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
